b812bdac47b242f86d2e98af9d0d0d5f800117d7
Leonardo Dino <leonardo@kunstdesign.com.br>
2016-08-02 19:33:26 -0300

    expose constants

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var SIP_CONSTRUCTOR = require('sip.js/src/SIP');
+var SIPjsPackage = require('sip.js/package.json');
 var checkEnv = require('./checkEnv');
 
 module.exports = {
   SIP: SIP_CONSTRUCTOR,
+  C: require('sip.js/src/Constants')(SIPjsPackage.name, SIPjsPackage.version),
   ENVIRONMENT: function(){
     return require('sip.js/src/environment');
   },
