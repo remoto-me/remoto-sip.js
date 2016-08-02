1f9c20c7068835d779918bfff738b14484274bca
Leonardo Dino <leonardo@kunstdesign.com.br>
2016-08-02 19:24:56 -0300

    on demand enviroment gathering
    avoid throing errors

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var SIP_CONSTRUCTOR = require('sip.js/src/SIP');
-var SIP_ENVIRONMENT = require('sip.js/src/environment');
 var checkEnv = require('./checkEnv');
 
 module.exports = {
   SIP: SIP_CONSTRUCTOR,
-  ENVIRONMENT: SIP_ENVIRONMENT,
+  ENVIRONMENT: function(){
+    return require('sip.js/src/environment');
+  },
   checkEnv: checkEnv,
 }
