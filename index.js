a5886cb6bf180c2a5b9e4b0f9c9b4a7da89c6789
Leonardo Dino <leonardo@kunstdesign.com.br>
2016-08-02 16:38:30 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+var SIP_CONSTRUCTOR = require('sip.js/src/SIP');
+var SIP_ENVIRONMENT = require('sip.js/src/environment');
+
+module.exports = {
+  SIP: SIP_CONSTRUCTOR,
+  ENVIRONMENT: SIP_ENVIRONMENT,
+}
