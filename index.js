b1a82d6c122ee54eb8340f32477e1943a2285ed2
Leonardo Dino <leonardo@kunstdesign.com.br>
2016-08-02 19:23:47 -0300

    add checkEnv

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var SIP_CONSTRUCTOR = require('sip.js/src/SIP');
 var SIP_ENVIRONMENT = require('sip.js/src/environment');
+var checkEnv = require('./checkEnv');
 
 module.exports = {
   SIP: SIP_CONSTRUCTOR,
   ENVIRONMENT: SIP_ENVIRONMENT,
+  checkEnv: checkEnv,
 }
